ac0b81628bf626f33033dc191e96522132214f81
easymean <m2162003@gmail.com>
2021-04-19 00:15:44 +0900

    fix: divide serializer into retrieve and list

--- src/property/serializers.py
+++ src/property/serializers.py
@@ -3,16 +3,16 @@ from rest_framework import serializers
 from property.models import Category, Stage
 
 
-class CategoryViewSetSerializer(serializers.ModelSerializer):
+class CategoryRetrieveSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        exclude = [
-            "is_active",
-            "created_at",
-            "updated_at",
-            "description"
-        ]
-        read_only_fields = ["id", "created_at", "updated_at"]
+        fields = ("name", "description")
+
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ("id", "name",)
 
 
 class RelationCategorySerializer(serializers.ModelSerializer):
