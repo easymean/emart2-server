0b1332142a651b1b90a1adb0aee9513060570b64
easymean <m2162003@naver.com>
2021-04-26 13:00:46 +0900

    refactor: fix django-dotenv

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -5,11 +5,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
-from pathlib import Path
-import dotenv
 
-dotenv.read_dotenv()
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
